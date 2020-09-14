4376e52879a2996bdbcc206b40902e35fd310e06
TochaFh <62609161+TochaFh@users.noreply.github.com>
2020-09-14 12:29:58 -0300

    First commit
    New classlib (FastConsole) and new console (ConsoleTest)

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FastConsole;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FastConsole.Print("aaa");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FastConsole/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FastConsole
+{
+    public static class FastConsole
+    {
+        public static void Println(string value)
+        {
+            Console.WriteLine(value);
+        }
+        public static void Println(object value)
+        {
+            Println(value.ToString());
+        }
+
+        public static void Print(string value)
+        {
+            Console.Write(value);
+        }
+        public static void Print(object value)
+        {
+            Print(value.ToString());
+        }
+    }
+}
